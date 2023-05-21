6ebf3aec496c31ac0e2645ff6290c6c30b645693
Samuel Olagunju <94651716+samuelolagunju@users.noreply.github.com>
2023-05-21 10:34:32 -0400

    Added player node to game scene

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -3,29 +3,29 @@ using Godot;
 
 namespace Verflucht.Scripts
 {
-    public class Player : KinematicBody2D
-    {
-        [Export] public int Speed = 200;
+	public class Player : KinematicBody2D
+	{
+		[Export] public int Speed = 200;
 
-        private Vector2 _velocity;
+		private Vector2 _velocity;
 
-        private void MovePlayer()
-        {
-            _velocity = new Vector2
-            {
-                x = Convert.ToInt32(Input.IsActionPressed("MoveRight")) -
-                    Convert.ToInt32(Input.IsActionPressed("MoveLeft")),
-                y = Convert.ToInt32(Input.IsActionPressed("MoveDown")) -
-                    Convert.ToInt32(Input.IsActionPressed("MoveUp"))
-            };
+		private void MovePlayer()
+		{
+			_velocity = new Vector2
+			{
+				x = Convert.ToInt32(Input.IsActionPressed("MoveRight")) -
+					Convert.ToInt32(Input.IsActionPressed("MoveLeft")),
+				y = Convert.ToInt32(Input.IsActionPressed("MoveDown")) -
+					Convert.ToInt32(Input.IsActionPressed("MoveUp"))
+			};
 
-            _velocity = _velocity.Normalized() * Speed;
-        }
+			_velocity = _velocity.Normalized() * Speed;
+		}
 
-        public override void _PhysicsProcess(float delta)
-        {
-            MovePlayer();
-            _velocity = MoveAndSlide(_velocity);
-        }
-    }
+		public override void _PhysicsProcess(float delta)
+		{
+			MovePlayer();
+			_velocity = MoveAndSlide(_velocity);
+		}
+	}
 }
