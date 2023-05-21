12f157f885dce6a162a5b6cf71b9c78dfb437b8c
Samuel Olagunju <94651716+samuelolagunju@users.noreply.github.com>
2023-05-21 10:25:17 -0400

    Added player with basic movement

---
+++ Scripts/Player.cs
@@ -0,0 +1,31 @@
+using System;
+using Godot;
+
+namespace Verflucht.Scripts
+{
+    public class Player : KinematicBody2D
+    {
+        [Export] public int Speed = 200;
+
+        private Vector2 _velocity;
+
+        private void MovePlayer()
+        {
+            _velocity = new Vector2
+            {
+                x = Convert.ToInt32(Input.IsActionPressed("MoveRight")) -
+                    Convert.ToInt32(Input.IsActionPressed("MoveLeft")),
+                y = Convert.ToInt32(Input.IsActionPressed("MoveDown")) -
+                    Convert.ToInt32(Input.IsActionPressed("MoveUp"))
+            };
+
+            _velocity = _velocity.Normalized() * Speed;
+        }
+
+        public override void _PhysicsProcess(float delta)
+        {
+            MovePlayer();
+            _velocity = MoveAndSlide(_velocity);
+        }
+    }
+}
