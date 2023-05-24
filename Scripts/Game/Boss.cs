d81c838820e6d79a3509b29956fc559fa03b1984
Samuel Olagunju <94651716+samuelolagunju@users.noreply.github.com>
2023-05-24 17:19:38 -0400

    Moved bullet pattern into class

--- Scripts/Game/Boss.cs
+++ Scripts/Game/Boss.cs
@@ -4,23 +4,23 @@ namespace Verflucht.Scripts.Game
 {
 	public class Boss : Sprite
 	{
-		private const int Accel = 30;
-		private int _currspeed = 50;
+		private int _acceleration = 30;
+		private int _speed = 50;
+		private int _maxSpeed = 200; 
 		private readonly PackedScene _bullet = (PackedScene)ResourceLoader.Load("res://Scenes/NodeScenes/Bullet.tscn");
-		private static readonly float[] Pattern = {0, 15, 30, 45, 60, 75, 90, 105, 120, 135, 150, 165, 180, 195, 210, 225, 240, 255, 270, 285, 300, 315, 330, 345, 360};
 
 		private void OnFireTimerTimeout() => Fire();
 
 		private void Fire()
 		{
-			_currspeed += Accel;
-			if (_currspeed > 200)
-				_currspeed = 200;
-			foreach (float i in Pattern)
+			_speed += _acceleration;
+			if (_speed >= _maxSpeed)
+				_speed = _maxSpeed;
+			foreach (float i in BulletPatterns.AllDirections)
 			{
-				Bullet bullet = _bullet.Instance() as Bullet;
+				if (!(_bullet.Instance() is Bullet bullet)) continue;
 				bullet.Angle = i + Mathf.Rad2Deg(Rotation);
-				bullet.Speed = _currspeed;
+				bullet.Speed = _speed;
 				bullet.Position = Position + new Vector2(0 * Mathf.Cos(Rotation), 50 * Mathf.Sin(Rotation));
 				GetParent().AddChild(bullet);
 			}
