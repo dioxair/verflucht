1a6622b5419de696163541ec990f1d382b497d42
Samuel Olagunju <94651716+samuelolagunju@users.noreply.github.com>
2023-05-25 19:28:57 -0400

    (Finally) added speed property in AllDirectionsPattern

--- Scripts/Game/BulletPatterns.cs
+++ Scripts/Game/BulletPatterns.cs
@@ -9,6 +9,7 @@ public abstract class BulletPatterns
             0, 15, 30, 45, 60, 75, 90, 105, 120, 135, 150, 165, 180, 195, 210, 225, 240, 255, 270, 285, 300, 315, 330,
             345, 360
         },
+        Speed = 50,
         MaxSpeed = 200,
         AccelerationSpeed = 30
     };
